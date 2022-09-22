eba30316ae371ed2baad9f3471029386a4ef243c
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-09-22 19:06:08 +0900

    DB上でbuilding_numとroom_numの組み合わせに一意性制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2022_09_21_164211) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["building_num", "room_num"], name: "index_users_on_building_num_and_room_num", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
