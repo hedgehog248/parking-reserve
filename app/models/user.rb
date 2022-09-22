ab0a0f589ee21791d12a6ba25559110da3d69ba2
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-09-23 00:16:14 +0900

    passwordに半角英数字のみのバリデーションを追加、および不要なピリオドの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,14 @@ class User < ApplicationRecord
   end
 
   NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
-  NAME_REGEX_MSG = 'is invalid. Input full-width characters.'.freeze
+  NAME_REGEX_MSG = 'is invalid. Input full-width characters'.freeze
 
   with_options presence: true, format: {with: NAME_REGEX, message: NAME_REGEX_MSG} do
     validates :last_name
     validates :first_name
   end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX_MSG = 'is invalid. Include both alphabetical and numeric characters'
+  validates_format_of :password, with: PASSWORD_REGEX, message: PASSWORD_REGEX_MSG
 end
