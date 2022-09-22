6bf98404df0487ed058e0d31fc0c2346e001530c
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-09-22 19:11:28 +0900

    新規利用者の保存機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :building_num
+    validates :room_num, uniqueness: { scope: :building_num }
+  end
+
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+  NAME_REGEX_MSG = 'is invalid. Input full-width characters.'.freeze
+
+  with_options presence: true, format: {with: NAME_REGEX, message: NAME_REGEX_MSG} do
+    validates :last_name
+    validates :first_name
+  end
 end
