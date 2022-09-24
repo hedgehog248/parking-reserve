c73f3380ecf2e123fa7cced74a6f7e326be18e40
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-09-24 18:45:24 +0900

    利用者情報編集機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:building_num, :room_num, :last_name, :first_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:building_num, :room_num, :last_name, :first_name])
   end
 end
