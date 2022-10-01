b88f555c1cd71308e551815f34a8010b8d354c2d
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-10-01 19:37:42 +0900

    予約時間内に既に予約がある場合のバリデーションを追加

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,6 @@
 class Reservation < ApplicationRecord
   require 'active_support/all'
+  include ReservationsHelper
 
   belongs_to :user
 
@@ -13,6 +14,7 @@ class Reservation < ApplicationRecord
   validate :s_time_deadline_check
   validate :e_time_open_check
   validate :e_time_deadline_check
+  validate :check_overbooking
 
   private
 
@@ -34,4 +36,24 @@ class Reservation < ApplicationRecord
     e_time_deadline = start_datetime + 24.hours
     errors.add(:end_datetime, 'は開始日時の24時間以内を選択してください。') if end_datetime >= e_time_deadline
   end
+
+  def check_overbooking
+    list = get_group_list(start_datetime, park_num)
+    check_hour = start_datetime
+    flag = 0
+    while check_hour < end_datetime
+      list.each do |reserv|
+        result = check_hour.between?(reserv[:start_datetime], reserv[:end_datetime])
+        flag += 1 if result == true
+      end
+      check_hour += 1.hour
+    end
+    if flag > 0
+      errors.add(:base, :invalid, message: "指定の日時には既に他の予約が入っています。変更してください。")
+      return false
+    else
+      return true
+    end
+  end
+
 end
