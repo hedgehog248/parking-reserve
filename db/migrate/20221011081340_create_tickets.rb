d391c31f6be72bb289c74cee14393bd01602a14b
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-10-11 17:27:25 +0900

    マイグレーションとアソシエーションの設定

--- db/migrate/20221011081340_create_tickets.rb
+++ db/migrate/20221011081340_create_tickets.rb
@@ -1,7 +1,9 @@
 class CreateTickets < ActiveRecord::Migration[6.0]
   def change
     create_table :tickets do |t|
-
+      t.string     :destination
+      t.string     :car_model
+      t.references :reservations, null: false, foreign_keys: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_27_062944) do
+ActiveRecord::Schema.define(version: 2022_10_11_081340) do
 
   create_table "reservations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "park_num", null: false
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2022_09_27_062944) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "tickets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "destination"
+    t.string "car_model"
+    t.bigint "reservations_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["reservations_id"], name: "index_tickets_on_reservations_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
