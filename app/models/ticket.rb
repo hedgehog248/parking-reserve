08f57cf8baa2999231315023e06dc3f112d2cc97
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-10-16 01:00:13 +0900

    ticketモデルのバリデーションを再設定

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,13 @@
 class Ticket < ApplicationRecord
   belongs_to :reservation
+  has_one_attached :pdf
+
+  with_options presence: true do
+    validates :building_num
+    validates :room_num
+    validates :destination
+    validates :car_model
+    validates :license_num
+    validates :derivers_name
+  end
 end
