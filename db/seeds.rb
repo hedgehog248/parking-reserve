ce6411d6e6e6adb7b1ab07703ade740ece8eed6f
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-10-31 18:22:00 +0900

    userテーブルにadminカラムを追加、seedで管理者情報を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  building_num:          "管理事務所",
+  room_num:              "000",
+  last_name:             "管理人",
+  first_name:            "管理人",
+  email:                 ENV['ADMIN_EMAIL'],
+  password:              ENV['ADMIN_PASSWORD'],
+  password_confirmation: ENV['ADMIN_PASSWORD'],
+  admin:                 true
+)
