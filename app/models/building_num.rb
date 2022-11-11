9af68996fb264f436a5fdebef7fd4d034969e693
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-11-11 17:48:11 +0900

    AHモデルのidを振り直し（管理事務所番号を追加）

--- app/models/building_num.rb
+++ app/models/building_num.rb
@@ -1,13 +1,14 @@
 class BuildingNum < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' },
-    { id: 10, name: '10号棟' },
-    { id: 11, name: '11号棟' },
-    { id: 12, name: '12号棟' },
-    { id: 13, name: '13号棟' },
-    { id: 14, name: '14号棟' },
-    { id: 15, name: '15号棟' },
-    { id: 16, name: '16号棟' },
-    { id: 17, name: '17号棟' }
+    { id: 1, name: '10号棟' },
+    { id: 2, name: '11号棟' },
+    { id: 3, name: '12号棟' },
+    { id: 4, name: '13号棟' },
+    { id: 5, name: '14号棟' },
+    { id: 6, name: '15号棟' },
+    { id: 7, name: '16号棟' },
+    { id: 8, name: '17号棟' },
+    { id: 9, name: '管理事務所' }
   ]
 end
