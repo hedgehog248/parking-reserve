b061bc98a1a0b0fd390e18f82277d1ced036585c
hedgehog248 <atsushi.hasunuma02@gmail.com>
2022-11-11 16:34:18 +0900

    棟番号と部屋番号のリストを作成

--- app/models/building_num.rb
+++ app/models/building_num.rb
@@ -1,2 +1,13 @@
-class BuildingNum < ApplicationRecord
+class BuildingNum < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '---' },
+    { id: 10, name: '10号棟' },
+    { id: 11, name: '11号棟' },
+    { id: 12, name: '12号棟' },
+    { id: 13, name: '13号棟' },
+    { id: 14, name: '14号棟' },
+    { id: 15, name: '15号棟' },
+    { id: 16, name: '16号棟' },
+    { id: 17, name: '17号棟' }
+  ]
 end
